3fade4c8312574f44e11a2ab0c0c64e751e8d556
samglz10 <samglz10@gmail.com>
2023-08-15 22:17:09 -0400

    getting data values in a nested array

--- formSubmit.js
+++ formSubmit.js
@@ -1,11 +1,22 @@
 //grab the submit-btn by id
 
 const submitForm = document.querySelector("#myForm");
-
+//need window.onload so page will not read values of null because the js code is being executed before the DOM/function is ready 
+window.onload = () =>{
 // e = capture the event parameter
 submitForm.addEventListener('submit',(e) => {
-    //prevent the default behavior- stops the browser from trying to submit the form to another page 
+    //stops the browser from trying to submit the form by default
     e.preventDefault();
-    console.log('form submitted!')
-})
+    //itirate through the object to obtain valeus
+   for(const data of submitForm){
+    console.log(data.value)
+    const ObjectData = [data.name, data.value]
+    //getting entries in names into an array
+        console.log(ObjectData)
+        }
+        
+        
+    })
+    
+}
 
