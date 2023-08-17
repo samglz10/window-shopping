f9a15c88d15d8dbc1dd52c4642ac07182d7ad745
samglz10 <samglz10@gmail.com>
2023-08-16 20:05:33 -0400

    creating newProduct Object to send to fakestoreAPI

--- formSubmit.js
+++ formSubmit.js
@@ -1,22 +1,33 @@
-//grab the submit-btn by id
 
+//grab the HTML elements by id
 const submitForm = document.querySelector("#myForm");
+const titleData = document.querySelector('#title');
+const priceData = document.querySelector('#price');
+const descriptionData = document.querySelector('#description');
+const imageData = document.querySelector('#image');
+const categoryData =document.querySelector('#category');
+
+
+
+
 //need window.onload so page will not read values of null because the js code is being executed before the DOM/function is ready 
 window.onload = () =>{
 // e = capture the event parameter
 submitForm.addEventListener('submit',(e) => {
     //stops the browser from trying to submit the form by default
     e.preventDefault();
-    //itirate through the object to obtain valeus
-   for(const data of submitForm){
-    console.log(data.value)
-    const ObjectData = [data.name, data.value]
-    //getting entries in names into an array
-        console.log(ObjectData)
-        }
-        
-        
+    const data = new FormData(e.target.value); 
+    let newProductObject = {
+        title: titleData.value,
+        price: priceData.value,
+        description: descriptionData.value,
+        image: imageData.value,
+        category: categoryData.value
+
+    }
+    console.log(data)
     })
-    
+
 }
+
 
