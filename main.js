73f30536e6fe1b39dcf2ae74eef40f307a2c8c06
Azat Valiev <azat@valievventures.com>
2023-08-19 10:09:59 -0400

    Fixed rating styling
    Co-authored-by: Samuel Gonzalez <samglz10@gmail.com>

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ const renderProducts = async () => {
       //categoryElement.textContent = "Category " + result[i].category;
       //descriptionElement.textContent = result[i].description;
       priceElement.textContent = "$ "+result[i].price // priceStyle(result[i].price );
-      ratingElement.textContent = starRating(result[i].rating.rate);
+      ratingElement.innerHTML = starRating(result[i].rating.rate);
       //inventoryElement.textContent = 'Items in Stock: ' + result[i].rating.count;
       imgElement.src=result[i].image;
 
@@ -104,35 +104,28 @@ const renderProducts = async () => {
 
 renderProducts();
 
-// let foo: undefined
-/** @type {string} */
-let foo
-
 /** 
  * @param {number | undefined} rating
  * @returns {string} 
  * */
 function starRating(rating = 0) {
-  const star = '⭐';
-  const halfStar = '🌓'
-  const emptyStar = '★'
   let starRating = [];
   let roundedRating = roundToStep(rating, 0.5);
 
   // Count whole stars
   for(let i = 0; i < Math.floor(roundedRating) ; i++){
-    starRating.push(star);
+    starRating.push(FULL_STAR);
   }
 
   // Check for half stars
   if (roundedRating !== Math.round(rating)) {
-    starRating.push(halfStar);
+    starRating.push(HALF_STAR);
   }
 
   // Filling wiht empty stars
   const currentStars = starRating.length;
   for(let j = 0; j < 5 - currentStars; j++) {
-      starRating.push(emptyStar);
+      starRating.push(EMPTY_STAR);
   }
       
   return starRating.join('');
@@ -146,6 +139,21 @@ function roundToStep(num, step){
   return Math.round(num / step) * step
 }
 
+const HALF_STAR = `
+<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+  <path d="m22 9.24-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4V6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"></path>
+</svg>
+`;
+const FULL_STAR = `
+<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+  <path d="M12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+</svg>
+`;
+const EMPTY_STAR = `
+<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+  <path d="m22 9.24-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"></path>
+</svg>
+`;
 
 
 /*
